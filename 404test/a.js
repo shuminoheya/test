8493a8f13f1bdc2715a61824d4fda454ce923f02
nyankoromoti <datahozonnyou20110416@gmail.com>
2026-01-31 17:01:36 +0900

    Update a.js

--- 404test/a.js
+++ 404test/a.js
@@ -1,32 +1,31 @@
 (function () {
-    "use strict";
-
-    // Service Worker 登録
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/sw.js");
+        navigator.serviceWorker.register("sw.js").then(() => {
+            console.log("SW registered");
+        });
     }
 
-    // ボタンが押されたら SW にメッセージ送信
-    function setupButtons() {
-        const enableBtn = document.querySelector("[data-404-enable]");
-        const disableBtn = document.querySelector("[data-404-disable]");
-
-        if (enableBtn) {
-            enableBtn.addEventListener("click", () => {
-                if (confirm("本当に 404 モードを有効化しますか？")) {
-                    navigator.serviceWorker.controller?.postMessage("ENABLE_404");
-                    alert("404 モードを有効化しました");
-                }
-            });
-        }
-
-        if (disableBtn) {
-            disableBtn.addEventListener("click", () => {
-                navigator.serviceWorker.controller?.postMessage("DISABLE_404");
-                alert("404 モードを無効化しました");
+    function send(msg) {
+        if (navigator.serviceWorker.controller) {
+            navigator.serviceWorker.controller.postMessage(msg);
+        } else {
+            window.addEventListener("load", () => {
+                navigator.serviceWorker.controller?.postMessage(msg);
             });
         }
     }
 
-    document.addEventListener("DOMContentLoaded", setupButtons);
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelector("[data-404-enable]")?.addEventListener("click", () => {
+            if (confirm("本当に 404 を有効化しますか？")) {
+                send("ENABLE_404");
+                alert("404 を有効化しました。ページを再読み込みしてください。");
+            }
+        });
+
+        document.querySelector("[data-404-disable]")?.addEventListener("click", () => {
+            send("DISABLE_404");
+            alert("404 を無効化しました。ページを再読み込みしてください。");
+        });
+    });
 })();
